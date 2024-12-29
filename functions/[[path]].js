338daf34c4656ac9e4ccbbc2d03afd26b7de9862
anastasykropm <anastasykropese@gmail.com>
2024-12-29 07:33:01 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://aboba.co.ua'
+	const proxyUrl = 'https://fauiisnerberesex.xyz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
